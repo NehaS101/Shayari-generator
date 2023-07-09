7b3f1340275c59ddf9f5101597b5811e3c6bd747
NehaS101 <solankineha297@gmail.com>
2023-07-09 13:54:02 +0530

    setup ai configuration

--- Backend/index.js
+++ Backend/index.js
@@ -4,3 +4,25 @@ const axios = require('axios');
 const cors = require('cors');
 require("dotenv").config();
 
+const config = new Configuration({
+    apiKey:process.env.AI_Key
+})
+const api = new OpenAIApi(config);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/shayari',async(req,res)=>{
+    let keyword = req.query.keyword;
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port '+process.env.PORT);
+})
