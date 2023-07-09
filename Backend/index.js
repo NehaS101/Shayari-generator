26fabd33cf32f2a9641a3d93f71694096428e37b
NehaS101 <solankineha297@gmail.com>
2023-07-09 14:04:52 +0530

    shayari backend done

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const config = new Configuration({
     apiKey:process.env.AI_Key
 })
-const api = new OpenAIApi(config);
+const openai = new OpenAIApi(config);
 
 const app = express();
 
@@ -17,9 +17,22 @@ app.use(cors());
 app.post('/shayari',async(req,res)=>{
     let keyword = req.query.keyword;
     try {
-        
+        const prompt = async() =>{
+            const text = `Please provide a shayari that contains word ${keyword} in hindi`;
+            const response = await openai.createCompletion({
+                model:"text-davinci-003",
+                prompt:text,
+                max_token:500,
+                n:1
+            });
+            const generatedShayari = response.data.choices[0].text;
+
+            return generatedShayari
+        }
+        const shayari = await prompt();
+        res.send(shayari);
     } catch (error) {
-        
+        res.send({error: error.message});
     }
 })
 
