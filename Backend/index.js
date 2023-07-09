651131b79350db4e7aab0be8f1b17f5f3209c0dd
NehaS101 <solankineha297@gmail.com>
2023-07-09 13:36:59 +0530

    start shayari generator

---
+++ Backend/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const {Configuration,OpenAIApi} = require('openai');
+const axios = require('axios');
+const cors = require('cors');
+require("dotenv").config();
+
